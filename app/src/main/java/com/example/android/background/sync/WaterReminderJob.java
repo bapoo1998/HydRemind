7922afffc42f1b7d435642d1ddae5aa0b7d1e80e
Bapusaheb Patil <bapoo1998@gmail.com>
2017-10-07 21:11:47 +0530

    Creating Android-Job structure to replace FirebaseJobDispatcher

---
+++ app/src/main/java/com/example/android/background/sync/WaterReminderJob.java
@@ -0,0 +1,30 @@
+package com.example.android.background.sync;
+
+import android.support.annotation.NonNull;
+
+import com.evernote.android.job.Job;
+import com.evernote.android.job.JobRequest;
+
+import java.util.concurrent.TimeUnit;
+
+public class WaterReminderJob extends Job {
+
+    public static final String TAG = "water_reminder_job";
+
+    @NonNull
+    @Override
+    protected Result onRunJob(Params params) {
+        ReminderTasks.executeTask(getContext(), ReminderTasks.ACTION_CHARGING_REMINDER);
+        return Result.SUCCESS;
+    }
+
+    public static void scheduleJob() {
+        new JobRequest.Builder(WaterReminderJob.TAG)
+                .setRequiresCharging(true)
+                .setExecutionWindow(TimeUnit.MINUTES.toMillis(15), TimeUnit.MINUTES.toMillis(15) + TimeUnit.MINUTES.toMillis(30))
+                .setUpdateCurrent(true)
+                .build()
+                .schedule();
+    }
+
+}
